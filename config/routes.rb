3fb5451194dbceb53084d3b940e505a23d53a314
Dion H. Bajramaj <ddiioonn123@gmail.com>
2019-10-26 20:29:48 +0200

    vfin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users
 resources :products
+resources :categories
+
   get '/home', to: 'faqetstatike#home'
 get '/modelet', to: 'products#index'
 get '/signup', to: 'users#new'
