078d53820df59d248e752aabc54483fc16a0e9be
Dion H. Bajramaj <ddiioonn123@gmail.com>
2019-11-28 21:49:55 +0100

    Shitmi i integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190424141110) do
+ActiveRecord::Schema.define(version: 20191128204726) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
