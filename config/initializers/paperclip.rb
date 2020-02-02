c3b32d243d08e19bf4dcfc655380c6856a48e83f
Dion H. Bajramaj <ddiioonn123@gmail.com>
2020-02-02 21:26:34 +0100

    s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = 's3-eu-central-1.console.aws.amazon.com'
+Paperclip::Attachment.default_options[:url] = 'npshkodrabucket1.s3.console.aws.amazon.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
