e4c3082dbd5c76a6262a62490bb9df447e52309e
Dion H. Bajramaj <ddiioonn123@gmail.com>
2020-02-07 20:02:01 +0100

    imgs

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,6 @@ belongs_to :user
 belongs_to :category
 has_many :orders
 
- has_attached_file :product_img, styles: { product_index: "992x292>", :product_show => "992x292>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
-
-
 
 
 
